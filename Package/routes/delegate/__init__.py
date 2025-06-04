031edddba8b101b73e9614ed374b2b8c36500f88
Oussama Mahmoud <anadafor7@gmail.com>
2025-06-04 16:57:35 +0100

    Delete Package/routes directory

--- Package/routes/delegate/__init__.py
+++
@@ -1,6 +0,0 @@
-from .announcements import bp as announcements
-from .calendar import bp as calendar
-
-def init_app(delegate_bp):
-    delegate_bp.register_blueprint(announcements)
-    delegate_bp.register_blueprint(calendar)
